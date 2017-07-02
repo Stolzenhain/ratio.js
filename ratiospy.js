b79c660c59476dc96eaaf509a07800e0e93d7975
Florian Stolzenhain <info@stolzenhain.net>
2017-07-02 16:22:36 +0200

    JS: indentation

--- ratiospy.js
+++ ratiospy.js
@@ -3,49 +3,49 @@ var childSelector = '*:first-child';
 
 var childRatio = function() {
   
-var _self = this;
-
-this.update = function (){
-
-Array.prototype.forEach.call(frames, function( frame ){
+  var _self = this;
   
-  var child = frame.querySelectorAll(childSelector)[0];
+  this.update = function (){
   
-  //TODO: get frame inner content ratio
-  //TODO: update child ratio only when called w/ extra parameter
+    Array.prototype.forEach.call(frames, function( frame ){
+      
+      var child = frame.querySelectorAll(childSelector)[0];
+      
+      //TODO: get frame inner content ratio
+      //TODO: update child ratio only when called w/ extra parameter
+      
+      /* get float offsets incl. padding, but w/o margin */ 
+      offset = {
+        'frame' : frame.getBoundingClientRect(),
+        'child' : child.getBoundingClientRect()
+      }
+      
+      //remove existing classes
+      //TODO: use a more elegant solution
+      child.classList.remove('child-portrait');
+      child.classList.remove('child-landscape');
+    
+      if (
+        //frame ratio
+        offset.frame.width/offset.frame.height
+        <=
+        //child ratio
+        offset.child.width/offset.child.height
+      ) {
+        //frame is thinner or same ratio – child behaves as landscape
+        child.classList.add('child-landscape');
+      } else {
+        //frame is wider – child behaves as portrait
+        child.classList.add('child-portrait');
+      }
+      
+    });
   
-  /* get float offsets incl. padding, but w/o margin */ 
-  offset = {
-    'frame' : frame.getBoundingClientRect(),
-    'child' : child.getBoundingClientRect()
   }
   
-  //remove existing classes
-  //TODO: use a more elegant solution
-  child.classList.remove('child-portrait');
-  child.classList.remove('child-landscape');
-
-  if (
-    //frame ratio
-    offset.frame.width/offset.frame.height
-    <=
-    //child ratio
-    offset.child.width/offset.child.height
-  ) {
-    //frame is thinner or same ratio – child behaves as landscape
-    child.classList.add('child-landscape');
-  } else {
-    //frame is wider – child behaves as portrait
-    child.classList.add('child-portrait');
-  }
+  //init
+  _self.update();
   
-});
-
-}
-
-//init
-_self.update();
-
-return this;
+  return this;
 
 }
